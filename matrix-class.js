7413a8f6f5220c7ff99b403b1981cce8dfac663d
Fpffy <41022637+Fpffy@users.noreply.github.com>
2018-08-20 17:33:30 +0200

    update

--- matrix-class.js
+++ matrix-class.js
@@ -1,4 +1,5 @@
-const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 let incidenceMatStr =
 `001100001
